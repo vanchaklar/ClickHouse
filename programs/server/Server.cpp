6a13be95c0d8febe1e4c3b1f905240cdb4830ce0
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-03-20 20:47:06 +0100

    Missing directive

--- programs/server/Server.cpp
+++ programs/server/Server.cpp
@@ -2843,8 +2843,10 @@ try
             main_config_reloader.reset();
             access_control.stopPeriodicReloading();
 
+#if USE_SSL
             /// Stop ACME tasks.
             ACME::Client::instance().shutdown();
+#endif
 
             is_cancelled = true;
 
