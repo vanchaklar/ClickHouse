c90f59364868d444ec514ba48ed1172ba2c63ee8
Elmi Ahmadov <elmi.ahmadov@clickhouse.com>
2025-10-23 12:06:16 +0200

    fix tests

--- tests/queries/0_stateless/02346_text_index_cache.sql
+++ tests/queries/0_stateless/02346_text_index_cache.sql
@@ -2,6 +2,9 @@
 
 SET enable_analyzer = 1;
 SET allow_experimental_full_text_index = 1;
+SET use_skip_indexes = 1;
+SET use_skip_indexes_on_data_read = 1;
+SET query_plan_direct_read_from_text_index = 1;
 SET log_queries = 1;
 
 DROP TABLE IF EXISTS tab;
@@ -23,7 +26,7 @@ FROM numbers(512);
 DROP VIEW IF EXISTS text_index_dictionary_block_cache_stats;
 CREATE VIEW text_index_dictionary_block_cache_stats AS (
   SELECT
-    concat('cache_hits = ', toString(ProfileEvents['TextIndexDictionaryBlockCacheHits']), ', cache_misses = ', toString(ProfileEvents['TextIndexDictionaryBlockCacheMisses']))
+    concat('cache_hits = ', ProfileEvents['TextIndexDictionaryBlockCacheHits'], ', cache_misses = ', ProfileEvents['TextIndexDictionaryBlockCacheMisses'])
   FROM system.query_log
   WHERE query_kind ='Select'
       AND current_database = currentDatabase()
@@ -57,7 +60,7 @@ CREATE TABLE tab
 (
     id UInt32,
     message String,
-    INDEX idx(message) TYPE text(tokenizer = array, dictionary_block_size = 128)
+    INDEX idx(message) TYPE text(tokenizer = array)
 )
 ENGINE = MergeTree
 ORDER BY (id);
@@ -69,11 +72,11 @@ INSERT INTO tab SELECT number, 'text_pl_3' FROM numbers(1024);
 DROP VIEW IF EXISTS text_index_posting_list_cache_stats;
 CREATE VIEW text_index_posting_list_cache_stats AS (
   SELECT
-    concat('cache_hits = ', toString(ProfileEvents['TextIndexPostingListCacheHits']), ', cache_misses = ', toString(ProfileEvents['TextIndexPostingListCacheMisses']))
+    concat('cache_hits = ', ProfileEvents['TextIndexPostingListCacheHits'], ', cache_misses = ', ProfileEvents['TextIndexPostingListCacheMisses'])
   FROM system.query_log
   WHERE query_kind ='Select'
       AND current_database = currentDatabase()
-      AND match(trimRight(query), {filter:String})
+      AND endsWith(trimRight(query), concat('hasAnyTokens(message, [', {filter:String}, ']);'))
       AND type='QueryFinish'
   LIMIT 1
 );
@@ -82,13 +85,13 @@ SELECT '--- cache misses on the first query execution for tokens.';
 SELECT count() FROM tab WHERE hasAnyTokens(message, ['text_pl_1', 'text_pl_2']);
 
 SYSTEM FLUSH LOGS query_log;
-SELECT * FROM text_index_posting_list_cache_stats(filter = 'text_pl_1');
+SELECT * FROM text_index_posting_list_cache_stats(filter = '\'text_pl_1\', \'text_pl_2\'');
 
 SELECT '--- cache hits for the deserialized posting lists.';
 SELECT count() FROM tab WHERE hasAnyTokens(message, ['text_pl_2', 'text_pl_3']);
 
 SYSTEM FLUSH LOGS query_log;
-SELECT * FROM text_index_posting_list_cache_stats(filter = 'text_pl_3');
+SELECT * FROM text_index_posting_list_cache_stats(filter = '\'text_pl_2\', \'text_pl_3\'');
 
 DROP VIEW text_index_posting_list_cache_stats;
 DROP TABLE tab;
